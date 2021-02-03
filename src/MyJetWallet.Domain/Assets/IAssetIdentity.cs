b4fc1740baef6787c2f93c1f893cd32c5c95939b
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-02-03 13:42:51 +0200

    add asset and spot instrument identity

---
+++ src/MyJetWallet.Domain/Assets/IAssetIdentity.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyJetWallet.Domain.Assets
+{
+    public interface IAssetIdentity : IJetBrokerIdentity
+    {
+        /// <summary>
+        /// Uniq ID of asset into the broker 
+        /// </summary>
+        string Symbol { get; }
+    }
+
+    [DataContract]
+    public class AssetIdentity : IAssetIdentity
+    {
+        [DataMember(Order = 1)] public string BrokerId { get; set; }
+        [DataMember(Order = 2)] public string Symbol { get; set; }
+    }
+}
---
+++ src/MyJetWallet.Domain/Assets/ISpotInstrumentIdentity.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyJetWallet.Domain.Assets
+{
+    public interface ISpotInstrumentIdentity : IJetBrokerIdentity
+    {
+        /// <summary>
+        /// Uniq ID of spot instrument (asset pair) into the broker 
+        /// </summary>
+        string Symbol { get; }
+    }
+
+    [DataContract]
+    public class SpotInstrumentIdentity : ISpotInstrumentIdentity
+    {
+        [DataMember(Order = 1)]
+        public string BrokerId { get; }
+
+        [DataMember(Order = 2)]
+        public string Symbol { get; }
+    }
+}
