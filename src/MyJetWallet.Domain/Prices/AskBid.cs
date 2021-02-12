8d3adef1cff6447473333498e4e3dd0e2517ae31
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-02-12 13:03:43 +0200

    add askbid model

---
+++ src/MyJetWallet.Domain/Prices/AskBid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MyJetWallet.Domain.Prices
+{
+    [DataContract]
+    public class AskBid
+    {
+        [DataMember(Order = 1)]
+        public string Id { get; set; }
+
+        [DataMember(Order = 2)]
+        public DateTime DateTime { get; set; }
+
+        [DataMember(Order = 3)]
+        public double Bid { get; set; }
+
+        [DataMember(Order = 4)]
+        public double Ask { get; set; }
+
+        [DataMember(Order = 5)]
+        public string LiquidityProvider { get; set; }
+    }
+}
