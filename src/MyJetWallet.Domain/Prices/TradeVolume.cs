283bf94e26a4d408ce75867f1f16577278d8e5e8
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-03-01 17:48:16 +0200

    add trade-volume price model

---
+++ src/MyJetWallet.Domain/Prices/TradeVolume.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MyJetWallet.Domain.Prices
+{
+    [DataContract]
+    public class TradeVolume
+    {
+        [DataMember(Order = 1)]
+        public string Id { get; set; }
+
+        [DataMember(Order = 2)]
+        public DateTime DateTime { get; set; }
+
+        [DataMember(Order = 3)]
+        public double Price { get; set; }
+
+        [DataMember(Order = 4)]
+        public double Volume { get; set; }
+
+        [DataMember(Order = 5)]
+        public string LiquidityProvider { get; set; }
+    }
+}
