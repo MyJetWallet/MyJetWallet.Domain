a2065ff04cbe84cd3709039af019258e0573d237
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-03-05 14:54:03 +0200

    order book term

---
+++ src/MyJetWallet.Domain/Prices/OrderBook.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace MyJetWallet.Domain.Prices
+{
+    [DataContract]
+    public class OrderBook
+    {
+        [DataMember(Order = 1)] public string Symbol { get; set; }
+        [DataMember(Order = 2)] public List<OrderBookLevel> BuyLevels { get; set; }
+        [DataMember(Order = 3)] public List<OrderBookLevel> SellLevels { get; set; }
+        [DataMember(Order = 4)] public long LastSequenceId { get; set; }
+    }
+}
---
+++ src/MyJetWallet.Domain/Prices/OrderBookLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyJetWallet.Domain.Prices
+{
+    [DataContract]
+    public class OrderBookLevel
+    {
+        public OrderBookLevel(decimal price, decimal volume, long sequenceId)
+        {
+            Price = price;
+            Volume = volume;
+            SeqId = sequenceId;
+        }
+
+        public OrderBookLevel()
+        {
+        }
+
+        [DataMember(Order = 1)] public decimal Price { get; set; }
+        [DataMember(Order = 2)] public decimal Volume { get; set; }
+        [DataMember(Order = 3)] public decimal SeqId { get; set; }
+    }
+}
